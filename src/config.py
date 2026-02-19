c195e0eea94866de0bc5f0568253ff4ac41a190d
jocabernadas-oss <jocabernadas@gmail.com>
2026-02-19 00:07:37 -0300

    Add game configuration settings

---
+++ src/config.py
@@ -0,0 +1,14 @@
+# Game Configuration Settings
+
+# Set the number of players
+NUM_PLAYERS = 4
+
+# Set the range of numbers
+MIN_NUMBER = 1
+MAX_NUMBER = 75
+
+# Set the winning conditions
+WINNING_CONDITIONS = {
+    "Bingo": 5,
+    "Blackout": 75
+}
